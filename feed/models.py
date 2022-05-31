a2b97d03c51bf4f32d141a9fc2bd9d2418ef6350
deepak-parmar <dgparmar1999@rediffmail.com>
2022-05-31 22:06:09 +0530

    Return Post object as string
    Implement __str__ (dundar method) to return text attribute as string

--- feed/models.py
+++ feed/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    text = models.CharField(max_length=280)
+	text = models.CharField(max_length=280)
+
+	def __str__(self):
+		return self.text
