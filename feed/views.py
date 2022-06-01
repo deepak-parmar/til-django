09eccca466690abde3c49a76e8ed9d2cef4e6291
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-01 19:13:09 +0530

    Create view for index page
    - Use GET as HTTP method
    - Use Post model as model for the view
    - Order queries by date
    - Assign `index.html` as the template

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class IndexView(ListView):
+    http_method_names = ["get"]
+    model = Post
+    context_object_name = "posts"
+    # order posts by date
+    queryset = Post.objects.all.order("date")
+    template_name = "index.html"
