7cc573755d05eb70ea5e77ed9d85891a0019a03f
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-01 19:28:23 +0530

    Fix queryset assignment
    - Post.object.all is a method so it's **`Post.object.all()`**
    - and `.order_by()` is used for ordering queryset not just `.order()`

--- feed/views.py
+++ feed/views.py
@@ -7,5 +7,5 @@ class IndexView(ListView):
     model = Post
     context_object_name = "posts"
     # order posts by date
-    queryset = Post.objects.all.order("date")
+    queryset = Post.objects.all().order_by("date")
     template_name = "index.html"
