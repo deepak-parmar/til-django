62aed82f309888845d367c13dd31f0cf6f3d190b
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-01 20:38:19 +0530

    Fix dateModified field
    Replace `auto_now_add` with just `auto_now` to store modified date.

--- feed/models.py
+++ feed/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     content = models.CharField(max_length=280)
     dateCreated = models.DateTimeField(auto_now_add=True)
-    dateModified = models.DateTimeField(auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
