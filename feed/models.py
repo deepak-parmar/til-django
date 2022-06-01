a067c8c3633c8b81c156f25c0f6b9a0b82c5c174
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-01 20:16:16 +0530

    Reformat

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class Post(models.Model):
-	content = models.CharField(max_length=280)
-	dateCreated = models.DateTimeField(auto_now_add=True)
-	dateModified = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.content
+class Post(models.Model):
+    content = models.CharField(max_length=280)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
