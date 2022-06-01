acb83f5b3f60e201d5f7be0c98ba30191a0f3ac0
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-01 20:14:16 +0530

    Change date field
    Drop `date` field and create two new field to store the date when post is created and when post is las modified.

--- feed/models.py
+++ feed/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 	content = models.CharField(max_length=280)
-	date = models.DateTimeField(auto_now=True)
+	dateCreated = models.DateTimeField(auto_now_add=True)
+	dateModified = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.content
