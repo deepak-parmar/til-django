faeb722f3fd34fdbeb62df130314fea0e2639828
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-02 17:03:04 +0530

    Create function to format dates
    Create `formatDate()` to format dates

--- feed/views.py
+++ feed/views.py
@@ -2,10 +2,37 @@ from django.views.generic import ListView
 from .models import Post
 
 
+# ? Worked without importing date from datetime
+def formatDate(date):
+    # if post was created today
+    if date.date() == date.today().date():
+        return f"Today · {date.strftime('%I:%M %p')}"
+    # if post was created yesterday
+    elif date.strftime("%d%m%Y") == date.today().strftime(
+        f"{date.today().day-1:02d}%m%Y"
+    ):
+        return f"Yesterday · {date.strftime('%I:%M %p')}"
+    # if post was created within the current year
+    elif date.year == date.today().year:
+        return date.strftime("%B %d · %I:%M %p")
+    else:
+        # show full format
+        return date.strftime("%B %d, %Y · %I:%M %p")
+
+
 class IndexView(ListView):
     http_method_names = ["get"]
+    template_name = "index.html"
     model = Post
     context_object_name = "posts"
-    # order posts by date
-    queryset = Post.objects.all().order_by("date")
-    template_name = "index.html"
+    queryset = []
+
+	# Format every date
+    for post in Post.objects.all().order_by("dateCreated"):
+		# check if post is modified
+        if post.dateCreated == post.dateModified:
+            post.dateModified = False
+        else:
+            post.dateModified = formatDate(post.dateModified)
+        post.dateCreated = formatDate(post.dateCreated)
+        queryset.append(post)
