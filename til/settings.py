b34ecfa25f0970634e80aa03fea34ed7ca26bf06
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-02 17:26:36 +0530

    Add missing comma

--- til/settings.py
+++ til/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # enable multi-sites
-    'django.contrib.sites'
+    'django.contrib.sites',
     
     # local apps
     'feed',
