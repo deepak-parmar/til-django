6be355a6390ddbab1d1578019c7f7099c1770ec6
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-02 17:52:58 +0530

    Add path to allauth URLs
    Use of `url()` from `django.conf.urls` is deprecated, `re_path()` is used instead.

--- til/urls.py
+++ til/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from feed import urls as feed_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
+    re_path("", include("allauth.urls"))
 ]
