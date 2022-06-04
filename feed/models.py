ca7f06a2f8c205fba4fe1c63cba0b971199aa578
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-04 18:23:15 +0530

    Create author field
    Create new field `author` in Post model and create Foreign key reference to User model.

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     content = models.CharField(max_length=280)
     dateCreated = models.DateTimeField(auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
