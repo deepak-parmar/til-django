c12383eaf31be181136d85a978dca7504fd42ae2
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-04 23:06:38 +0530

    Add path to PostCreateView

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PostCreateView, PostDetailView
 
 app_name = "feed"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:pk>/", PostDetailView.as_view(), name="view"),
+    path("post/", PostCreateView.as_view(), name="post"),
 ]
