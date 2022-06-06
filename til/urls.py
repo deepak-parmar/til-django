83a92507eaff380671780de52d704ffbfda4caa6
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-06 11:41:09 +0530

    Add path to user app and static assets

--- til/urls.py
+++ til/urls.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from feed import urls as feed_urls
+from user import urls as user_urls
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
-    re_path("", include("allauth.urls"))
-]
+    path("user/", include(user_urls, namespace="user")),
+    re_path("", include("allauth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
