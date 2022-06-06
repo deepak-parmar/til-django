52c7777bcccecf30ecea1d677f026a434f09fc0f
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-06 11:39:47 +0530

    Create ProfileDetailView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth.models import User
+from feed.models import Post
 
-# Create your views here.
+class ProfileDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "profile.html"
+    model = User
+    context_object_name = "profile"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_post_count"] = Post.objects.all().filter(author=self.get_object()).count()
+        # context["total_follower_count"] = Follower.objects.all().filter(author=self.get_object()).count()
+        return context
+    
