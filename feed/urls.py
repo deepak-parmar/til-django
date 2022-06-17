ca0449fe259e432519f55a58f24f7f79845938b8
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-17 11:01:50 +0530

    Add path to PostExploreView

--- feed/urls.py
+++ feed/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import IndexView, PostCreateView, PostDetailView
+from .views import IndexView, PostCreateView, PostDetailView, PostExploreView
 
 app_name = "feed"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("explore/", PostExploreView.as_view(), name="explore"),
     path("<int:pk>/", PostDetailView.as_view(), name="view"),
     path("post/", PostCreateView.as_view(), name="post"),
 ]
