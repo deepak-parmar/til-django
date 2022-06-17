36679640b57d2f932292e8986404dcb3c688f75e
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-17 22:53:34 +0530

    Add path to ProfileUsernameUpdateView

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileDeleteView, ProfileDetailView, FollowView, ProfileNameUpdateView
+from .views import ProfileDeleteView, ProfileDetailView, FollowView, ProfileNameUpdateView, ProfileUsernameUpdateView
 
 app_name = "user"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<str:username>/follow/", FollowView.as_view(), name="follow"),
     path("delete/<str:username>/", ProfileDeleteView.as_view(), name="delete"),
     path("update-name/<str:username>/", ProfileNameUpdateView.as_view(), name="update-name"),
+    path("update-username/<str:username>/", ProfileUsernameUpdateView.as_view(), name="update-username"),
 ]
