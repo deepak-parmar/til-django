4973c4e3ed3b23f4a08d997e1f8d90c065f06818
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-17 22:23:01 +0530

    Add paths to FollowView and ProfileDeleteView

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProfileDetailView
+from .views import ProfileDeleteView, ProfileDetailView, FollowView
 
 app_name = "user"
 
 urlpatterns = [
     path("<str:username>/", ProfileDetailView.as_view(), name="profile"),
+    path("<str:username>/follow/", FollowView.as_view(), name="follow"),
+    path("delete/<str:username>/", ProfileDeleteView.as_view(), name="delete"),
 ]
