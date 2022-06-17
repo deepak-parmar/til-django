26b10b885db3c4faa3b269ef4f16202eb174aeae
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-17 21:07:54 +0530

    Add path to PostDeleteView

--- feed/urls.py
+++ feed/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, PostCreateView, PostDetailView, PostExploreView
+from .views import IndexView, PostCreateView, PostDeleteView, PostDetailView, PostExploreView
 
 app_name = "feed"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("explore/", PostExploreView.as_view(), name="explore"),
     path("<int:pk>/", PostDetailView.as_view(), name="view"),
     path("post/", PostCreateView.as_view(), name="post"),
+    path("delete/<int:pk>", PostDeleteView.as_view(), name="delete"),
 ]
