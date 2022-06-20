82c10c23fb9c6477d67340408d47cd6a59eb8b7e
Deepak Parmar <dgparmar1999@rediffmail.com>
2022-06-20 12:40:27 +0530

    Use env. variable for SECRET_KEY

--- til/settings.py
+++ til/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!e#royx&vdpm%5z90npfx1nfp03s#5c*t(f6t_f&)4na9ss7is"
+SECRET_KEY = $SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
