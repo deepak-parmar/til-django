f7605ebf83b4b95334f5e6b90a6d332de5ed3414
deepak-parmar <dgparmar1999@rediffmail.com>
2022-06-20 12:38:58 +0530

    End URLs with slash

--- feed/urls.py
+++ feed/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("explore/", PostExploreView.as_view(), name="explore"),
     path("<int:pk>/", PostDetailView.as_view(), name="view"),
     path("post/", PostCreateView.as_view(), name="post"),
-    path("delete/<int:pk>", PostDeleteView.as_view(), name="delete"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="delete"),
 ]
